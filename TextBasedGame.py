99f97be119f07911e2d433bc2884ccd337e10b85
Nick Mancari <nmancari@cloudflare.com>
2022-02-18 07:52:28 -0600

    cosmetic touches

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -6,7 +6,7 @@ GREEN = '\033[92m'
 NC = '\033[0m'
 BOLD = '\033[1m'
 
-#Dictionary for locations, movement, and items in rooms
+# Dictionary for locations, movement, and items in rooms
 rooms = {
         'Entrance' : { 'East' : 'Dungeon Staircase' },
         'Dungeon Staircase' : { 'North' : 'Stalagmite Cove', 'South' : 'Forge', 'East' : 'Fire Cavern',  'West' : 'Entrance',  'Item' : 'Pearl' },
@@ -14,13 +14,13 @@ rooms = {
         'Troll Lair' : { 'West' : 'Stalagmite Cove' },
         'Forge' : { 'North' : 'Dungeon Staircase', 'East' : 'Icy Room', 'West' : 'Ore Mine', 'Item' : 'Sword' },
         'Icy Room' : { 'West' : 'Forge', 'Item' : 'Gem' },
-        'Ore Mine' : { 'East' : 'Forge', 'Item' : 'Coins' },
+        'Ore Mine' : { 'East' : 'Forge', 'Item' : 'Coin' },
         'Fire Cavern' : { 'North' : 'Gold Mine', 'West' : 'Dungeon Staircase', 'Item' : 'Ruby' },
         'Gold Mine' : { 'South' : 'Fire Cavern', 'Item' : 'Doubloon' }
 
 }
 
-#Game start instructions to educate the player on objectives and gameplay
+# Game start instructions to educate the player on objectives and gameplay
 def instructions() :
         print("\n----------------------------------------------")
         print('{:^45s}'.format(BOLD+"~~~~DUNGEONS & TROLLS~~~~"+NC))
@@ -30,14 +30,14 @@ def instructions() :
         print("Maneuver the dungeonscape with these commands: North, South, East, and West.")
         print("To add items to your inventory: Answer the prompt Yes of No\n")
 
-#Function to handle villain room situation
+# Function to handle villain room situation
 def villain_place(item_list):
 
     damage_roll = randint(1, 10)
 
     if 'Sword' in item_list:
         if damage_roll >= 3:
-            print("\n**You have defeated the troll!**\n")
+            print(BOLD+"\n**You have defeated the troll!**\n"+NC)
             print(GREEN+"\nYOU WIN!\n"+NC)
             exit()
         else:
@@ -47,7 +47,7 @@ def villain_place(item_list):
     else:
         print("You don't have a sword! Run away!")
 
-#Function to update player on current status as they move through the dungeons and add items to their inventory
+# Function to update player on current status as they move through the dungeons and add items to their inventory
 def player_status(place, inventory) :
 
     print("\nYou are in the "+place)
@@ -64,9 +64,9 @@ inventory = list()
 item = ''
 
 while True:
-    #player_status func
+    # player_status func at start of every loop
     player_status(place, inventory)
-    #check room for item and enage user on item collection logic
+    # Check room for item and enage user on item collection logic
     if 'Item' in rooms[place]:
         item = rooms[place]['Item']
         print("You see a {}".format(item))
@@ -75,10 +75,11 @@ while True:
         if collect_item == 'Yes':
             inventory.append(item)
             if len(inventory) == 7:
-                print(GREEN+"\nYOU WIN!\n"+NC)
+                print(inventory)
+                print(GREEN+"\nYOU WIN! You collected all 7 items!\n"+NC)
                 exit()
             del rooms[place]['Item']
-    #player movement logic
+    # Player movement logic
     player_input = input("------------\nEnter your move:\n")
     if player_input in rooms[place]:
         place = rooms[place][player_input]
